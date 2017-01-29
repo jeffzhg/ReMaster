6dd911233b13cf42e6f887efc5740fbc639ae165
Jeff <jeffzhg@gmail.com>
2017-01-28 23:15:16 -0800

    Distributed Systems PS1
    Find word definition in dictionary

---
+++ Code/src/com/homework/Dictionary.java
@@ -0,0 +1,104 @@
+package com.homework;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Dictionary {
+
+	private static final String defaultDictFilename = "data/dictionary.txt";
+	private String dictFilename;
+	private static BufferedReader dictionaryReader;
+	
+	
+	public Dictionary() {
+		super();
+		dictFilename = defaultDictFilename;
+		try {
+			setBr(new BufferedReader(new FileReader(dictFilename)));
+		} catch (FileNotFoundException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Dictionary(String dictFilename, BufferedReader br) {
+		super();
+		this.dictFilename = dictFilename;
+		this.setBr(br);
+	}
+
+	protected String findDefinitionInDict(String word)	{
+		String definition = "";
+		StringBuilder sb = new StringBuilder();
+		
+		//Enough bad memory, let's iterate through the file line by line instead of loading it all in
+		String line = null;
+		long lineCount = 0;
+		
+		try {
+			while (null != (line = dictionaryReader.readLine())) {
+				lineCount ++;
+				
+				// Find line that matches exactly the word
+				// Ideally line contains only one word && is in ALL CAP
+				if (isWordLine(line)
+						&& 0 == line.compareToIgnoreCase(word)){
+
+					System.out.println("Found definition for:" + word);
+
+					// Capture the following paragraph starting with Defn:
+					String nextLine = ";";
+					Boolean recordDef = false;
+					
+					while (null != (nextLine = dictionaryReader.readLine())) {
+						if (nextLine.startsWith("Defn:")) {
+							System.out.println("Start recording definition for " + word);
+							recordDef = true;							
+						}
+						if (recordDef && nextLine.isEmpty()) {
+							System.out.println("Finished recording definition for " + word);
+							recordDef = false;
+							return sb.toString();
+						}
+						if (recordDef) {
+							sb.append(nextLine);							
+						}
+					}
+					
+					//matchingLineNumber = lineCount;
+					
+					// Other optimizations: Capture all meanings of this word.
+					
+					System.out.println("Recording dictionary line#" + lineCount);
+				}
+				System.out.println("Searching dictionary line#" + lineCount);
+			}				
+			
+		} catch (IOException e) {
+
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return definition;
+	}
+	
+	private Boolean isWordLine(String line) {
+		String trimmedLine = line.trim();
+		String[] segments = trimmedLine.split(" ");
+		return (1 == segments.length
+				&& trimmedLine.equals(trimmedLine.toUpperCase()));
+	}
+
+	public BufferedReader getBr() {
+		return dictionaryReader;
+	}
+
+	public void setBr(BufferedReader br) {
+		this.dictionaryReader = br;
+	}
+
+
+}
---
+++ Code/src/com/homework/Worker.java
@@ -0,0 +1,94 @@
+package com.homework;
+import java.util.Scanner;
+
+public class Worker {
+	private static Scanner inputScanner;
+	private static String inputText;
+		
+	public Worker() {
+		super();
+		inputScanner = new Scanner(System.in);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Worker(Scanner inputScanner) {
+		super();
+		this.inputScanner = inputScanner;
+	}
+
+	public static void work(){
+		try {
+			System.out.println("Eclipse could work!");	
+			doSearchWork();		
+		} catch (Exception ex) {
+			System.out.println("Catch all is a false sense of security.");
+		}
+		
+		
+	}
+
+	public static void defineWord(String query) throws InterruptedException {
+		
+		try {
+
+			String definition = "";
+			Dictionary dt = new Dictionary();
+			definition = dt.findDefinitionInDict(inputText);
+
+			if ("" == definition) {
+				System.out.println("Dictionary isn't large enough, can't find definition for: " + inputText);
+			} else {
+				System.out.println("Summarizing meaning for " + inputText);
+				for (int i = 3; i > 0; i--) {
+					System.out.println(i);
+					Thread.sleep(1000);
+				}
+				System.out.println(query + " means the following:");
+				System.out.println(definition);
+			}
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+			System.out.println("Sorry I was interrupted, where were we?");
+		}
+		
+		
+	}
+	
+	
+	public static void doSearchWork() throws InterruptedException {
+		
+		try {
+
+			while (true) {
+				
+				System.out.println("Give me a word, I can find it for you.");
+				inputText = inputScanner.nextLine().toString();
+				System.out.println("Searching, sit tight...");
+				
+				for (int i = 3; i > 0 ; i--) {
+					Thread.sleep(1000);
+					System.out.println(i);
+				}
+				
+				WordBag wb = new WordBag();
+				long location = wb.findWordInMyBag(inputText);
+	
+				if (-1 == location) {
+					System.out.println("Didn't find the word" + inputText);
+				} else {
+					System.out.println("Found it!");
+					System.out.println(inputText + " was hiding at line #" + location);
+					defineWord(inputText);
+					break;
+				}
+			}
+			
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+			System.out.println("Sorry I was interrupted, where were we?");
+		}
+		
+		
+	}
+	
+}
